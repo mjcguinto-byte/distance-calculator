91f979671dee643b3991e14b570ad641cfef8d63
mjcguinto-byte <mjcguinto@brc.pshs.edu.ph>
2026-08-10 11:46:52 +0800

    Added comments
    Added comments to clarify input requirements for distance calculation.

--- Guinto_Menandro_DistanceProgram.py
+++ Guinto_Menandro_DistanceProgram.py
@@ -4,5 +4,7 @@ x1 = float(input("Enter x1: "))
 y1 = float(input("Enter x2: "))
 x2 = float(input("Enter y1: "))
 y2 = float(input("Enter y2: "))
+#Input the 4 values in order for the calculator to get the distance
+
 d = math.sqrt((pow(x2-x1, 2) + pow(y2-y1, 2)))
 print("The distance is", round(d, 2))
