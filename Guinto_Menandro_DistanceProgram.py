de5e5968384d79fdc5f12dae0ad6ef458b892a93
mjcguinto-byte <mjcguinto@brc.pshs.edu.ph>
2026-08-10 11:17:44 +0800

    Add files via upload

---
+++ Guinto_Menandro_DistanceProgram.py
@@ -0,0 +1,8 @@
+import math 
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter x2: "))
+x2 = float(input("Enter y1: "))
+y2 = float(input("Enter y2: "))
+d = math.sqrt((pow(x2-x1, 2) + pow(y2-y1, 2)))
+print("The distance is", round(d, 2))
